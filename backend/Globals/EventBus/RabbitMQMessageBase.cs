2cc1c4a5c448caa37b27b3ed6f497493899e1bd1
Інна Шпонька <inneta3@gmail.com>
2026-01-05 14:16:17 +0200

    Add project Globals, which contain base types for other projects

---
+++ backend/Globals/EventBus/RabbitMQMessageBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Globals.EventBus
+{
+    public class RabbitMQMessageBase
+    {
+        public string Sender { get; private set; }
+        public string EventType { get; private set; }
+        public string Data { get; set; }  // Само сообщение или данные
+
+        public DateTime TimeSend { get; private set; } = DateTime.UtcNow;
+
+        public RabbitMQMessageBase(string sender, string eventType, string data)
+        {
+            Sender = sender;
+            EventType = eventType;
+            Data = data;
+        }
+    }
+}
---
+++ backend/Globals/Models/RefBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Globals.Models
+{
+    public class RefBase<T> : EntityBase where T : EntityBase
+    {
+        public int RefID { get; set; }
+        public T Parent { get; set; }
+    }
+}
