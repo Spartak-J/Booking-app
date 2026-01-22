2cf26c7bc5fd4bd17892d3367d86289e72aa0f96
Інна Шпонька <inneta3@gmail.com>
2026-01-22 18:56:39 +0200

    add ReviewApiService

---
+++ backend/booking/ReviewApiService/Service/Interface/IReviewService.cs
@@ -0,0 +1,16 @@
+﻿using Globals.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using ReviewApiService.Models;
+using ReviewApiService.View;
+
+namespace ReviewApiService.Service.Interface
+{
+    public interface IReviewService : IServiceBase<Review>
+    {
+        Task<List<Review>> GetReviewsByOfferId(int offerId);
+        Task<List<Review>> GetReviewsByUserId(int userId);
+        Task<double> GetRatingByOfferId(int offerId);
+
+        //Task<Dictionary<int, OfferReviewStats>> GetOfferReviewStatsAsync(IEnumerable<int> offerIds);
+    }
+}
